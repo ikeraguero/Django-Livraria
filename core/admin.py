b95bc8412c778e7215644d939e438ba5b2678f88
ikeraguero <ikerpires407@gmail.com>
2022-08-07 15:13:52 -0300

    first commit

---
+++ core/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from core.models import Categoria, Editora, Autor, Livro
+admin.site.register(Categoria)
+admin.site.register(Editora)
+admin.site.register(Autor)
+admin.site.register(Livro)
+
+
