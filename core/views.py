9635ce0b3d14d4f7e7a8a814ac6c7be794712d12
ikeraguero <ikerpires407@gmail.com>
2022-08-29 19:46:25 -0300

    Class 13

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework.viewsets import ModelViewSet
+## from rest_framework.permissions import isAuthenticated
 
 from core.models import Categoria, Editora, Autor, Livro
 from core.serializer import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroDetailSerializer, LivroSerializer
@@ -10,6 +11,7 @@ from core.serializer import CategoriaSerializer, EditoraSerializer, AutorSeriali
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+##    permission_classes = [isAuthenticated]
 
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
