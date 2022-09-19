d58b612ce667db192388006bf4865801a3acef72
-add <-add>
2022-09-19 14:51:02 -0300

    update

--- livraria/settings.py
+++ livraria/settings.py
@@ -134,8 +134,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissions',
-    ]
-}
+# REST_FRAMEWORK = {
+#    'DEFAULT_PERMISSION_CLASSES': [
+#        'rest_framework.permissions.DjangoModelPermissions',
+#    ]
+#  }
