650e98d872581edd36325bca09e7c83d8e8ac74f
ikeraguero <ikerpires407@gmail.com>
2022-09-24 12:11:47 -0300

    Class 17

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
 
+class UsuarioAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "cpf", "telefone", "data_nascimento")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
 # Register your models here.
 
 from core.models import Categoria, Editora, Autor, Livro
@@ -7,5 +26,7 @@ admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Autor)
 admin.site.register(Livro)
+
+
 
 
--- livraria/settings.py
+++ livraria/settings.py
@@ -38,10 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'rest_framework',
-    'core',
     "rest_framework_simplejwt",
+    'core',
 
 ]
 
@@ -145,3 +144,6 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
+
+AUTH_USER_MODEL = "core.Usuario"
+
