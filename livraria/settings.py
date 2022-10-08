93b2e1e5d1d3eca14054e034280b53bcafc7cf59
ikeraguero <ikerpires407@gmail.com>
2022-10-08 09:48:35 -0300

    Removing Authentication

--- livraria/settings.py
+++ livraria/settings.py
@@ -138,13 +138,13 @@ CORS_ALLOW_ALL_ORIGINS = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        # 'rest_framework.permissions.IsAuthenticated',
-        "rest_framework.permissions.DjangoModelPermissions",
-    ],
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+   # "DEFAULT_PERMISSION_CLASSES": [
+   #     # 'rest_framework.permissions.IsAuthenticated',
+   #     "rest_framework.permissions.DjangoModelPermissions",
+   # ],
+   # "DEFAULT_AUTHENTICATION_CLASSES": (
+   #     "rest_framework_simplejwt.authentication.JWTAuthentication",
+    #),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
