fff221ecc94711bb923428eb652851f6b2417d6a
ikeraguero <ikerpires407@gmail.com>
2022-12-01 15:54:03 -0300

    PythonAnywhere

--- livraria/settings.py
+++ livraria/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 import environ
-...
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Carrega as variáveis de ambiente do sistema operacional e as prepara para usá-las
 env = environ.Env()
 environ.Env.read_env((os.path.join(BASE_DIR, '.env')))
@@ -23,8 +26,6 @@ DEBUG = env('DEBUG')
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 DATABASES = {'default': env.db()}
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
